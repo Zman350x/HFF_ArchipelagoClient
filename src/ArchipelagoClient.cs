8dbb20531c3b35d83ca7294f977516d3b024f2f5
Zman350x <zpnewman10@gmail.com>
2026-03-01 18:30:56 -0500

    Initial commit

---
+++ src/ArchipelagoClient.cs
@@ -0,0 +1,24 @@
+﻿namespace HffArchipelagoClient
+{
+    using BepInEx;
+
+    [BepInPlugin("top.zman350x.hff.archipelagoclient", "Human: Fall Flat Archipelago Client", "0.0.1")]
+    [BepInProcess("Human.exe")]
+    public sealed class ArchipelagoClient : BaseUnityPlugin
+    {
+        public static ArchipelagoClient instance;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void OnDestroy()
+        {
+        }
+
+        private void Start()
+        {
+        }
+    }
+}
