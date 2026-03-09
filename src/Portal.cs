0092b46c9b70f809776e25d56436df50e8695b81
Zman350x <zpnewman10@gmail.com>
2026-03-09 18:33:12 -0400

    Finish adding return portals

--- src/Portal.cs
+++ src/Portal.cs
@@ -202,60 +202,60 @@ namespace HffArchipelagoClient
                     rotation = new Vector3(0.0f, 180.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/LumberAssets/Lumber.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(6.0f, 0.0f, 7.5f);
+                    rotation = new Vector3(0.0f, 90.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/RedRockAssets/RedRock.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-40.35f, -2.795f, -38.43f);
+                    rotation = new Vector3(0.0f, 270.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/TowerAssets/Tower.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(80.16f, 43.5194f, 53.45f);
+                    rotation = new Vector3(0.0f, 167.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/MiniatureAssets/Miniature.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-8.5f, 0.0f, -9.2f);
+                    rotation = new Vector3(0.0f, 180.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/CopperWorldAssets/CopperWorld.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-5.95f, 0.0f, -6.75f);
+                    rotation = new Vector3(0.0f, 180.0f, 0.0f);
                     break;
                 case "Assets/ContestLevels/NavalAssests/Naval_Ben.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(65.7f, 2.6427f, -5.6f);
+                    rotation = new Vector3(0.0f, 259.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/UnderwaterAssets/OceanAdventure.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-4.87f, 15.23f, 28.47f);
+                    rotation = new Vector3(351.0f, 69.6124f, 350.681f);
 					break;
                 case "Assets/ContestLevels/DockyardAssets/Dockyard.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-63.545f, -4.2234f, -43.23f);
+                    rotation = new Vector3(0.0f, 180.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/MuseumAssets/Museum.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(38.3f, 0.85f, 27.5f);
+                    rotation = new Vector3(0.0f, 90.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/HikeAssets/Scenes/Hike.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-110.77f, -35.025f, -128.93f);
+                    rotation = new Vector3(0.0f, 331.4221f, 0.0f);
 					break;
                 case "Assets/ContestLevels/CandylandAssets/Candyland.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-279.5f, 52.96f, -158.92f);
+                    rotation = new Vector3(359.0f, 353.0f, 6.0f);
 					break;
                 case "Assets/ContestLevels/FacilityAssets/Facility.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-62.75f, 31.0f, 67.25f);
+                    rotation = new Vector3(0.0f, 235.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/Punk/SteamPunk.unity":
                     position = new Vector3(-62.78f, -4.585f, -42.0f);
                     rotation = new Vector3(0.0f, 315.0f, 0.0f);
 					break;
                 case "Assets/ContestLevels/VikingAssets/Viking.unity":
-                    position = new Vector3(0.0f, 0.0f, 0.0f);
-                    rotation = new Vector3(0.0f, 0.0f, 0.0f);
+                    position = new Vector3(-75.66f, 0.53f, -32.46f);
+                    rotation = new Vector3(0.0f, 215.0f, 2.0f);
                     break;
 
                 // Lobbies
